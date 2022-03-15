3d65dabe9b0ff054ba318cb98c01f29966699aa0
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-03-15 10:54:11 -0500

    feat: employee class
    This commit has introduced the employee class to handle data regarding
    to the name, start and end working time and the day.

---
+++ src/tools/employee.py
@@ -0,0 +1,8 @@
+class Employee(object):
+    """docstring for Employee"""
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def set_start_time(self, start_time: str):
+        self.start_time = start_time
